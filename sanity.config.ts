55aece3e0140a18b3f676a6c9b75052d6df2c096
ryanfurrh <ryan@commoncurriculum.com>
2023-10-03 00:51:45 -0400

    Add sanity-media-browser and phosphor

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,7 @@
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...index]]/page.tsx` route
  */
-
+import {media } from "sanity-plugin-media"
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
@@ -21,5 +21,6 @@ export default defineConfig({
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
+    media(),
   ],
 })
