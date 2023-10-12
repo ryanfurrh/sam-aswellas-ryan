d229c43fb7cdfb5a9c890b7222fe35124580537c
ryanfurrh <ryan@commoncurriculum.com>
2023-10-11 21:43:17 -0400

    Move time into interval

--- src/components/RelationshipCounter.tsx
+++ src/components/RelationshipCounter.tsx
@@ -8,21 +8,19 @@ export default function RelationshipCounter() {
   const [seconds, setSeconds] = useState(0);
   useEffect(() => {
     const target = new Date("11/05/2020 19:37:00");
-    const interval = setInterval(() => {}, 1000);
-    const now = new Date();
-    const difference = now.getTime() - target.getTime();
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = now.getTime() - target.getTime();
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
 
-    const d = Math.floor(difference / (1000 * 60 * 60 * 24));
-    setDays(d);
-    const h = Math.floor(
-      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    setHours(h);
-    const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    setMinutes(m);
-
-    const s = Math.floor((difference % (1000 * 60)) / 1000);
-    setSeconds(s);
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
+    }, 1000);
 
     return () => clearInterval(interval);
   }, []);
