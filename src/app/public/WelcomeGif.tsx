8e1e04708ada644f5af6532ee33e89d4d9cd18d1
ryanfurrh <ryan@commoncurriculum.com>
2023-10-21 03:02:34 -0400

    Add welcome gif

---
+++ src/app/public/WelcomeGif.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function WelcomeGif() {
+  return (
+    <Image
+      src="https://web.archive.org/web/20091019202444if_/http://www.geocities.com/setyo79/pic/welcome.gif"
+      alt="Welcome Gif with butterflies ♡"
+      width={300}
+      height={10}
+    />
+  );
+}
