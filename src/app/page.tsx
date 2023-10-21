3481706fba6a0c212f04658c1f61fc5d78826df5
ryanfurrh <ryan@commoncurriculum.com>
2023-10-21 03:06:41 -0400

    Add welcome gif to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import HomeNav from "@/components/HomeNav";
+import WelcomeGif from "./public/WelcomeGif";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 w-full gap-12">
-      <h1 className="text-3xl font-bold">Home</h1>
+    <main className="flex min-h-screen flex-col items-center p-12 pt-28 w-full gap-4">
+      <WelcomeGif />
       <HomeNav />
     </main>
   );
