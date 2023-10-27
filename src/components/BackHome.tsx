8ed3070a451e59608a0901654affc685900839fd
ryanfurrh <ryanfurrh@gmail.com>
2023-10-27 18:28:58 -0400

    Fix back home color props

--- src/components/BackHome.tsx
+++ src/components/BackHome.tsx
@@ -3,17 +3,22 @@
 import { useRouter } from "next/navigation";
 import { HouseLine } from "phosphor-react";
 
-export default function BackHome() {
+export default function BackHome(props: { color: string | undefined }) {
   const router = useRouter();
   return (
-    <div className="absolute ">
+    <div className="">
       <button
         onClick={() => {
           router.push("/");
         }}
-        className="text-start bg-neutral-900/50 rounded-lg px-2 mx-16 py-2 hover:bg-neutral-800 hover:cursor-pointer"
+        className="text-start bg-neutral-900/50 rounded-lg px-2 mx-6 sm:mx-16 py-2 hover:bg-neutral-800 hover:cursor-pointer"
       >
-        <HouseLine width={20} height={20} weight="regular" color="green" />
+        <HouseLine
+          width={20}
+          height={20}
+          weight="regular"
+          className={`${props.color}`}
+        />
       </button>
     </div>
   );
