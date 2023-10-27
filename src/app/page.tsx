61590741e811a1e768767b62271069b407ef724c
ryanfurrh <ryanfurrh@gmail.com>
2023-10-27 18:30:10 -0400

    Add gif to homepage & fix positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,11 @@ import WelcomeGif from "./public/WelcomeGif";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-12 pt-28 w-full gap-4">
-      <WelcomeGif />
+    <main className="flex min-h-screen flex-col items-center px-12 py-10 w-full gap-4">
+      <div className="">
+        <WelcomeGif />
+        <div className="bg-white absolute w-full h-24 top-12 right-0 left-0 hue"></div>
+      </div>
       <HomeNav />
     </main>
   );
