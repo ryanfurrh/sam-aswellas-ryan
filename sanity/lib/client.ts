f7a8b4662028892f1f789f867d3363bf511c8049
ryanfurrh <ryanfurrh@gmail.com>
2023-11-01 02:05:13 -0400

    Add revalidate secret to sanity client

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,12 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import { apiVersion, dataset, projectId, revalidateSecret } from "./sanity.api";
 
 export const client = createClient({
   apiVersion,
   dataset,
   projectId,
-  useCdn,
-})
+  useCdn: false,
+
+  perspective: "published",
+});
