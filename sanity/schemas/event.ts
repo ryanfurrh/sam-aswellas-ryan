d992d82857757c09e115f909da8f8d7ac9b1a61d
ryanfurrh <ryanfurrh@gmail.com>
2023-11-01 02:06:33 -0400

    Create event schema

--- sanity/schemas/event.ts
+++ sanity/schemas/event.ts
@@ -1,5 +1,35 @@
+import { Confetti } from "@phosphor-icons/react/dist/ssr/Confetti";
+
 export default {
   name: "event",
-  type: "date",
   title: "Event",
+  type: "document",
+  icon: Confetti,
+  fields: [
+    {
+      name: "title",
+      title: "Event Name",
+      type: "string",
+    },
+    {
+      name: "date",
+      title: "Date",
+      type: "date",
+    },
+    {
+      name: "icon1",
+      title: "Icon 1",
+      type: "icon",
+    },
+    {
+      name: "icon2",
+      title: "Icon 2",
+      type: "icon",
+    },
+    {
+      title: "Location",
+      name: "string",
+      type: "string",
+    },
+  ],
 };
